83bbf0a0ead39aa2a319fce4763a032fd07cf5a3
Sonechka M <s212288s@gmail.com>
2020-03-12 08:25:28 -0400

    new

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Lamb Burger');
+INSERT INTO burgers (name) VALUES ('Turkey Burger');
+INSERT INTO burgers (name) VALUES ('Salmon Burger');
+INSERT INTO burgers (name) VALUES ('Juicy Lucy');
+INSERT INTO burgers (name) VALUES ('Ramen Burger');
